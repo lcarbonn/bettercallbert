4b9a4de959f67e21b24e69bc6a2458cee5da1fa9
lcarbonn <laurent.carbonnaux@gmail.com>
2022-12-09 16:29:12 +0100

    add card dialog

--- components/domain/card-detail.vue
+++ components/domain/card-detail.vue
@@ -1,36 +1,50 @@
 <template>
-    <md-card md-with-hover
-             v-if="card">
-        <md-card-header>
-            <span class="md-title">{{card.title}}</span>
-        </md-card-header>
-        <md-card-actions md-alignment="space-between">
-            <md-button class="md-icon-button"
-                       v-if="previousId"
-                       :href="'/cards/'+previousId">
-                <md-icon>navigate_before</md-icon>
-            </md-button>
-            <md-button v-if="card.link"
-                       :href="card.link"
-                       target="_blank"
-                       class="md-primary">Jump to source</md-button>
-            <md-button class="md-icon-button"
-                       v-if="nextId"
-                       :href="'/cards/'+nextId">
-                <md-icon>navigate_next</md-icon>
-            </md-button>
-        </md-card-actions>
-        <md-card-media v-if="img">
-            <img :title="card.title"
-                 :alt="card.title"
-                 :src="img">
-        </md-card-media>
-    </md-card>
+    <div>
+        <md-card md-with-hover
+                 v-if="card">
+            <md-card-header>
+                <span class="md-title">{{ card.title }}</span>
+            </md-card-header>
+            <md-card-actions md-alignment="space-between">
+                <md-button class="md-icon-button"
+                           v-if="previousId"
+                           :href="'/cards/' + previousId">
+                    <md-icon>navigate_before</md-icon>
+                </md-button>
+                <md-button v-if="card.link"
+                           :href="card.link"
+                           target="_blank"
+                           class="md-primary">Jump to source</md-button>
+                <md-button class="md-icon-button"
+                           v-if="nextId"
+                           :href="'/cards/' + nextId">
+                    <md-icon>navigate_next</md-icon>
+                </md-button>
+            </md-card-actions>
+            <md-card-media v-if="img">
+                <img :title="card.title"
+                     :alt="card.title"
+                     :src="img"
+                     @click="showDialogIn = true">
+            </md-card-media>
+        </md-card>
+        <CardDialog v-if="img"
+                    :title="card.title"
+                    :img="img"
+                    :showDialogIn="showDialogIn"
+                    @closeDialog="closeDialog" />
+
+    </div>
 </template>
 
 <script>
+import CardDialog from '~/components/domain/card-dialog';
+
 
 export default {
+    components: {
+        CardDialog
+    },
 
     props: {
         card: {
@@ -50,6 +64,9 @@ export default {
             default: null
         }
     },
+    data: () => ({
+        showDialogIn: false
+    }),
     mounted() {
         this.setupListeners()
     },
@@ -76,6 +93,9 @@ export default {
         },
         swipeLeftHandler(e) {
             if (this.nextId) this.$router.push('/cards/' + this.nextId)
+        },
+        closeDialog() {
+            this.showDialogIn = false
         }
     }
 }
@@ -89,11 +109,13 @@ export default {
     vertical-align: top;
     text-align: center;
 }
+
 .md-card-media img {
     width: unset;
     max-height: 60vh;
     max-width: 100vw;
 }
+
 a {
     font-size: 1rem;
     letter-spacing: 0px;
@@ -104,6 +126,7 @@ a {
 a:hover {
     color: white;
 }
+
 @media (max-width: 400px) {
     .md-card {
         margin: 4px;
@@ -112,6 +135,7 @@ a:hover {
         text-align: center;
         width: -webkit-fill-available;
     }
+
     .md-card-media img {
         width: 100%;
         max-height: 60vh;
---
+++ components/domain/card-dialog.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <md-dialog :md-active.sync="showDialog">
+            <img v-if="img"
+                 :title="title"
+                 :alt="title"
+                 :src="img">
+        </md-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardDialog',
+    props: {
+        title: {
+            type: String,
+            default: null
+        },
+        img: {
+            type: String,
+            default: null
+        },
+        showDialogIn: {
+            type: Boolean,
+            default: false
+
+        }
+    },
+    computed: {
+        showDialog: {
+            // getter
+            get() {
+                return this.showDialogIn
+            },
+            // setter
+            set(newValue) {
+                this.closeDialog()
+            }
+        }
+    },
+    methods: {
+        closeDialog() {
+            this.$emit('closeDialog')
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+img {
+    height: 80vh;
+    max-width: 100vw;
+}
+</style>
