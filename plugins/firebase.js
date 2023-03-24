7612f89bfcde61b32585d3447a6fb046185e120f
lcarbonn <laurent.carbonnaux@gmail.com>
2023-03-24 12:35:47 +0100

    add firebase config

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,18 +4,7 @@ import { getAnalytics } from "firebase/analytics"
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 import { getAuth } from "firebase/auth"
-
-// Initialize Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyC7stjdwwDpBBBa_YX51teGYwQcfQ2oA_I",
-    authDomain: "bettercallbert.firebaseapp.com",
-    databaseURL: "https://bettercallbert.firebaseio.com",
-    projectId: "bettercallbert",
-    storageBucket: "bettercallbert.appspot.com",
-    messagingSenderId: "110642051311",
-    appId: "1:110642051311:web:48c6b911e8086ff8bbc31a",
-    measurementId: "G-T2R7DS2NB1"
-};
+import { firebaseConfig } from "./firebaseConfig.js";
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
