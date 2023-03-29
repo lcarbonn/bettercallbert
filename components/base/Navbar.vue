3b74ea75d30a77700c6e0672e8b8e09fda8adc74
lcarbonn <laurent.carbonnaux@gmail.com>
2023-03-29 11:19:44 +0200

    navbar fill

--- components/base/Navbar.vue
+++ components/base/Navbar.vue
@@ -9,27 +9,27 @@
                 src="~/static/icon.png"></b-avatar> BetterCallBert
     </b-navbar-brand>
 
-    <b-navbar-nav small
-                  v-if="themes"
-                  class="d-flex flex-row flex-wrap justify-content-start">
-      <b-nav-item v-for="theme in themes"
-                  :key="theme.id"
-                  :id="theme.id"
-                  :active="isActive(theme.id)"
-                  @click="filterCards(theme.id)"
-                  href="#"
-                  class="pr-1">
-        <span style="border-bottom-style: solid;"
-              :class="getThemeColor(theme)">
-          <!-- <span> -->
-          {{ theme.title.toUpperCase() }}</span>
-      </b-nav-item>
-    </b-navbar-nav>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse"
                 is-nav>
+      <b-navbar-nav small
+                    fill
+                    v-if="themes">
+        <b-nav-item v-for="theme in themes"
+                    :key="theme.id"
+                    :id="theme.id"
+                    :active="isActive(theme.id)"
+                    @click="filterCards(theme.id)"
+                    href="#"
+                    class="pr-1">
+          <span style="border-bottom-style: solid;"
+                :class="getThemeColor(theme)">
+            <!-- <span> -->
+            {{ theme.title.toUpperCase() }}</span>
+        </b-nav-item>
+      </b-navbar-nav>
       <b-navbar-nav small
                     class="ml-auto">
         <b-nav-form>
