3634dd5e733d6aef0a71ab6bc657af197e8463e4
lcarbonn <laurent.carbonnaux@gmail.com>
2026-01-27 10:22:44 +0100

    fixed overlay footer

--- app/components/base/Footer.vue
+++ app/components/base/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="fixed pt-2 bottom-0 left-0 right-0 z-40 backdrop-blur-lg bg-white/80 dark:bg-gray-900/80 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]">
     <USeparator type="dashed" color="primary"
       :avatar="{
         src: '/icon-64x64.png'
